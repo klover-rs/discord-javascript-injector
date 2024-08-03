808f7005d0c8fbeb571c3782c29711b7ccfaddd5
mari-rs <joelkerbusch@gmail.com>
2024-08-03 15:23:39 +0200

    added tests

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,18 @@ pub mod inject;
 mod constants;
 pub mod eject;
 
+#[cfg(test)]
+mod tests {
+    use check_installation::check_installed_clients;
+
+    use super::*;
+
+    #[test]
+    fn check_installed_clients_test() {
+        let installed_clients = check_installed_clients().unwrap();
+
+        for client in installed_clients {
+            println!("client: {:?}", client);
+        }
+    }
+}
