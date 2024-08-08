d1c6a69320f282e00f17f29ce436e616ba4f1feb
xzi <joelkerbusch@gmail.com>
2024-08-08 15:07:59 +0200

    updated asar.rs with ws features

--- src/asar.rs
+++ src/asar.rs
@@ -1,7 +1,7 @@
 use std::{fs::{self, File}, path::PathBuf};
 use asar::{AsarReader, AsarWriter, HashAlgorithm, Result as AsarResult};
 use walkdir::WalkDir;
-
+use rayon::prelude::*;
 use anyhow::Result as AnyResult;
 
 pub fn pack_asar(path: &PathBuf, dest: &PathBuf) -> AnyResult<()> {
@@ -26,13 +26,54 @@ pub fn pack_asar(path: &PathBuf, dest: &PathBuf) -> AnyResult<()> {
     Ok(())
 }
 
-pub fn extract_asar(asar_file: &PathBuf, output: &PathBuf) -> AsarResult<String> {
+pub fn extract_asar(asar_file: &PathBuf, output: &PathBuf) -> AsarResult<()> {
+    let asar_file = fs::read(asar_file)?;
+    let asar = AsarReader::new(&asar_file, None)?;
+
+
+    let total_files = asar.files().len() as f64;
+    let counter = std::sync::atomic::AtomicUsize::new(0);
+
+
+    asar.files().par_iter().try_for_each(|(path, file_info)| -> AsarResult<()> {
+        let output_path = output.join(path);
+
+        if let Some(parent) = output_path.parent() {
+            fs::create_dir_all(parent)?;
+        }
+
+        
+        let content = file_info.data();
+        fs::write(&output_path, content)?;
+        let current_count = counter.fetch_add(1, std::sync::atomic::Ordering::SeqCst) + 1;
+
+        let progress_percentage = (current_count as f64 / total_files) * 100.0;
+        println!("extracted: {} | {:.2}%", output_path.display(), progress_percentage);
+
+        Ok(())
+    })?;
+
+    println!("total files: {}", total_files);
+
+    Ok(())
+}
+
+use tokio_tungstenite::tungstenite::Message;
+use futures_util::sink::SinkExt;
+use tokio_tungstenite::WebSocketStream;
+use tokio::net::TcpStream;
+use tokio_tungstenite::MaybeTlsStream;
+pub async fn extract_asar_ws(asar_file: &PathBuf, output: &PathBuf, ws_stream: &mut WebSocketStream<MaybeTlsStream<TcpStream>>) -> AsarResult<String> {
+
+
+    
     let asar_file = fs::read(asar_file)?;
     let asar = AsarReader::new(&asar_file, None)?;
 
     let total_files = asar.files().len() as f64;
     let mut counter = 0;
 
+    
     for (path, file_info) in asar.files() {
         let output_path = output.join(path);
 
@@ -47,6 +88,9 @@ pub fn extract_asar(asar_file: &PathBuf, output: &PathBuf) -> AsarResult<String>
         let progress_percentage = (counter as f64 / total_files) * 100.0;
 
         println!("extracted: {} | {:.2}%", output_path.display(), progress_percentage);
+
+        let progress_message = format!("extracted: {} | {:.2}%", output_path.display(), progress_percentage);
+        ws_stream.send(Message::Text(progress_message)).await.unwrap();
     }
 
     println!("total files: {}", total_files);
