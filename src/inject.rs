832ba3f432bb16a49d0e2913b9c7f30c95033070
xzi <joelkerbusch@gmail.com>
2024-08-08 15:54:02 +0200

    updated inject.rs

--- src/inject.rs
+++ src/inject.rs
@@ -6,7 +6,59 @@ use crate::{asar::*, constants::{CORE_ASAR_BACKUP_FILE, CORE_ASAR_FILE}, targets
 
 use tokio_tungstenite::connect_async;
 
-pub async fn inject_ws(which_discord: &str, javascript_to_inject: &str, ws_url: &str) -> Result<()> {
+use swc_common::{input::SourceFileInput, SourceMap};
+use swc_ecma_ast::*;
+use swc_ecma_codegen::{text_writer::JsWriter, Emitter};
+use swc_ecma_parser::{lexer::Lexer, Parser, StringInput, Syntax};
+use swc_ecma_transforms_typescript::strip_type;
+use swc_ecma_visit::VisitMutWith;
+use swc_common::FileName;
+
+use std::rc::Rc;
+
+fn gen_javascript(content: &str) -> String {
+    let cm = SourceMap::new(swc_common::FilePathMapping::empty());
+
+    let fm = cm.new_source_file(Rc::new(FileName::Custom("inline".into())), content.into());
+
+    let lexer = Lexer::new(
+        Syntax::Typescript(Default::default()),
+        EsVersion::latest(),
+        StringInput::from(&*fm),
+        None,
+    );
+
+    let mut parser = Parser::new_from(lexer);
+    let mut module = parser
+        .parse_module()
+        .expect("failed to parse input as a module");
+
+
+    let lcm: std::rc::Rc<SourceMap> = cm.into();
+
+    module.visit_mut_with(&mut strip_type());
+
+    let code = {
+        let mut buf = Vec::new();
+        {
+            let mut emitter = Emitter {
+                cfg: Default::default(),
+                cm: lcm.clone(),
+                comments: None,
+                wr: JsWriter::new(lcm.clone(), "\n", &mut buf, None),
+            };
+
+            emitter.emit_module(&module).unwrap();
+        }
+
+        String::from_utf8_lossy(&buf).to_string()
+    };
+
+    code
+}
+
+
+pub async fn inject_ws(which_discord: &str, javascript_to_inject: &str, is_typescript: bool, ws_url: &str) -> Result<()> {
 
     let url = url::Url::parse(&ws_url)?;
     let (mut ws_stream, _) = connect_async(url).await?;
@@ -60,9 +112,19 @@ pub async fn inject_ws(which_discord: &str, javascript_to_inject: &str, ws_url: 
 
             extract_asar_ws(&path.join(CORE_ASAR_FILE), &dest_path, &mut ws_stream).await?;
 
-            inject_javascript("inject.js", &javascript_to_inject, &dest_path.join("app"))?;
+            let javascript_content = if is_typescript {
+                gen_javascript(&javascript_to_inject)
+            } else {
+                javascript_to_inject.to_string()
+            };
 
-            pack_asar(&dest_path, &path.join(CORE_ASAR_FILE))?;
+            println!("javascript content: {}\ntrue: {}", javascript_content, is_typescript);
+
+            tokio::time::sleep(Duration::from_secs(10)).await;
+
+            inject_javascript("inject.js", &javascript_content, &dest_path.join("app"))?;
+
+            pack_asar_ws(&dest_path, &path.join(CORE_ASAR_FILE), &mut ws_stream).await?;
 
             fs::remove_dir_all(&dest_path)?;
 
@@ -85,7 +147,7 @@ pub async fn inject_ws(which_discord: &str, javascript_to_inject: &str, ws_url: 
     Ok(())
 }
 
-pub fn inject(which_discord: &str, javascript_to_inject: &str) -> Result<()> {
+pub fn inject(which_discord: &str, javascript_to_inject: &str, is_typescript: bool) -> Result<()> {
 
     let targets = targets::get_discord_path();
     
@@ -94,6 +156,8 @@ pub fn inject(which_discord: &str, javascript_to_inject: &str) -> Result<()> {
     } else {
         return Err(anyhow!("couldnt find target client path"))
     };
+
+    
 
 
     match search_file(&target_client, CORE_ASAR_FILE) {
@@ -111,7 +175,15 @@ pub fn inject(which_discord: &str, javascript_to_inject: &str) -> Result<()> {
 
             extract_asar(&path.join(CORE_ASAR_FILE), &dest_path)?;
 
-            inject_javascript("inject.js", &javascript_to_inject, &dest_path.join("app"))?;
+            let javascript_content = if is_typescript {
+                gen_javascript(&javascript_to_inject)
+            } else {
+                javascript_to_inject.to_string()
+            };
+
+      
+
+            inject_javascript("inject.js", &javascript_content, &dest_path.join("app"))?;
 
             pack_asar(&dest_path, &path.join(CORE_ASAR_FILE))?;
 
@@ -183,3 +255,5 @@ fn inject_into_mainscreen(main_screen_path: &PathBuf, target_string: &str, new_c
 
     Ok(())
 }
+
+
